70dcf220246ee85f481719d2d620b0edbc604a96
klype <mikkelfossum@gmail.com>
2023-01-06 20:14:52 +0100

    Added comments

--- menus/death.gd
+++ menus/death.gd
@@ -1,10 +1,10 @@
 extends Control
 
 
-
+# When the play again button is pressed it loads back into the main world
 func _on_play_again_pressed():
 	get_tree().change_scene("res://worlds/castle/testing_world.tscn")
 
-
+# Not implemented yet
 func _on_quit_pressed():
 	pass
--- worlds/castle/door/door.gd
+++ worlds/castle/door/door.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+# Signal is a good alternative to pythons await, it can emit a signal so other scripts
+# can know if something happend, without the hassle of setting up export variables.
 signal door_ready
 
 signal left_door_entered
@@ -18,16 +20,8 @@ onready var door_right_hitbox = $DoorRight/HB/CS
 onready var door_up_hitbox    = $DoorUp/HB/CS
 onready var door_down_hitbox  = $DoorDown/HB/CS
 
-#var left_door_visible
-#var right_door_visible
-#var up_door_visible
-#var down_door_visible
-#
-#var left_door_locked
-#var right_door_locked
-#var up_door_locked
-#var down_door_locked
-
+# This function checks sets the doors and their hitboxes visible or invisible 
+# depending on what the world code does.
 func update_doors():
 	
 	door_left.visible  = get_parent().left_door_visible
@@ -42,17 +36,25 @@ func update_doors():
 	
 
 func _ready():
+	update_doors() # Runs when the scene is fully loaded
+
+func _process(delta): # Runs for every frame
 	update_doors()
 
-func _process(delta):
-	update_doors()
 
+# Checks if the body variable has the metadata (player = true) It will return true if it does.
 func check_player(body):
 	if body.has_meta("player"):
 		if body.get_meta("player") == true:
 			return true
 	return false
 
+# The emit_signal() sends a signal (Godot's take on await) that activates a function in another script
+# These functions are connected to the doors hitboxes and runs when hit. It uses body, which is the
+# equal to the body that hit. Then the check_player() function checks if what hit the door is a player,
+# and if that is the case, it sends a signal to the world script.
+# I am pretty satisfied with these functions. They look nice, run smooth and does what
+# theyre supposed to do.
 func door_up_entered(body):
 	if check_player(body) and door_up_hitbox.visible and door_up.visible:
 		print("door u")
